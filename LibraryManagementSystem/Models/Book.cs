46af500c094a6e5eee2128afdd50ae11ebd016f4
Ahmed Adel Hassan <aa3452000@gmail.com>
2026-07-19 03:29:19 +0300

    Book

--- LibraryManagementSystem/Models/Book.cs
+++ LibraryManagementSystem/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Models
 {
-    internal class Book
+    internal class Book : LibraryItem, ISearchable
     {
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+
+        public bool IsAvailable { get; set; }   
+
+        public Book(int id, string title,  string author, int year, string genre): base(id, title)
+        {
+            Author = author;
+            Year = year;
+            Genre = genre;
+            IsAvailable = true; 
+        }
+
+        public override string GetInfo()
+        {
+            string status = IsAvailable ? "Available" : "Borrowed";
+            return $"ID: {Id}, Title: {Title}, Author: {Author}," +
+                   $" Year: {Year}, Genre: {Genre}, Status: {status}";
+        }
+
+        public bool MatchesQuery(string query)
+        {
+            return Title.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                   Author.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                   Genre.Contains(query, StringComparison.OrdinalIgnoreCase);
+        }
     }
 }
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LibraryManagementSystem
+﻿using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book = new Book(1, "Clean Code", "Robert Martin", 2008, "Programming");
+            Console.WriteLine(book.GetInfo());
+            Console.WriteLine(book.MatchesQuery("clean"));
+
+
+            Console.ReadKey();
         }
+
     }
 }
