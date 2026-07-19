b8bd87c62ab17d85989610d296f2658a9752b590
Ahmed Adel Hassan <aa3452000@gmail.com>
2026-07-19 04:04:08 +0300

    Member

--- LibraryManagementSystem/Models/Member.cs
+++ LibraryManagementSystem/Models/Member.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,79 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Models
 {
-    internal class Member
+    internal class Member : ISearchable
     {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Email { get; set; }   
+        public DateTime JoinDate { get; }
+
+        public Book?[] BorrowedBooks { get; } = new Book?[10];
+
+        public virtual int MaxBorrowLimit => 5;
+        public virtual int LoanDays => 14;
+
+        public Member(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;    
+            Email = email;  
+            JoinDate = DateTime.Now;    
+        }
+        
+        public virtual string GetInfo()
+        {
+            return $"Regular Member - ID: {Id}, Name : {Name}, Email: {Email}";
+        }
+
+        public bool MatchesQuery(string query)
+        {
+            return Name.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                Email.Contains(query, StringComparison.OrdinalIgnoreCase);
+
+        }
+
+        public int GetBorrowedBooksCount()
+        {
+            int count = 0;
+            foreach (Book? book in BorrowedBooks)
+            {
+                if(book is not null)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public void AddBorrowedBook(Book book)
+        {
+            if(GetBorrowedBooksCount() >= MaxBorrowLimit)
+            {
+                throw new InvalidOperationException("Member has reached the " +
+                    "borrowing limit");
+            }
+
+            for (int i = 0; i<BorrowedBooks.Length; i++)
+            {
+                if (BorrowedBooks[i] is null)
+                {
+                    BorrowedBooks[i] = book;
+                    return;
+                }
+            }
+        }
+
+        public void RemoveBorrowedBook(int bookId)
+        {
+            for (int i = 0; i<BorrowedBooks.Length; i++)
+            {
+                if (BorrowedBooks[i]?.Id == bookId)
+                {
+                    BorrowedBooks[i] = null;
+                    return;
+                }
+            }
+                    
+        }
     }
 }
