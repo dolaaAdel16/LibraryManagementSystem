809f7ec55d6d4136c19e78f882a7e4f52f9487e8
Ahmed Adel Hassan <aa3452000@gmail.com>
2026-07-19 07:39:00 +0300

    program

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -10,11 +10,13 @@ namespace LibraryManagementSystem
             //Console.WriteLine(book.GetInfo());
             //Console.WriteLine(book.MatchesQuery("clean"));
 
-            Member member1 = new Member(1, "Ahmed", "ahmed@gmail.com");
-            Member member2 = new Member(2, "Adel", "adel@gmail.com");
+            //Member member1 = new Member(1, "Ahmed", "ahmed@gmail.com");
+            //Member member2 = new Member(2, "Adel", "adel@gmail.com");
 
-            Console.WriteLine(member1.GetInfo());
-            Console.WriteLine(member2.GetInfo());
+            //Console.WriteLine(member1.GetInfo());
+            //Console.WriteLine(member2.GetInfo());
+
+
 
 
 
